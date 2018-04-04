36abd75952b7dc44d2494c4e976fb23eecfe93b7
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-04-05 00:10:03 +0800

    add dockerfile

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 export default  {
     perPage: process.env.perPage || 10,
-    port: process.env.port || 3000,
+    port: process.env.port || 8080,
     secretKey: process.env.SECRET_KEY || 'secret' 
 }
